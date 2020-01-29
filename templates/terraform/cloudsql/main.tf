bc25921afe7f64c8b48be6a03cfd88c4dd38619a
Raaid Arshad <rarshad@broadinstitute.org>
2020-01-29 16:30:07 -0500

    start creating cloudsql module

---
+++ templates/terraform/cloudsql/main.tf
@@ -0,0 +1,60 @@
+# NOTE: This is needed because CloudSQL doesn't allow DB names to be reused
+# within the week that they're deleted, so whenever we want to tear down and
+# recreate the instance we need to generate a new name (vs waiting a week).
+resource random_id cloudsql_random_id {
+  byte_length = 8
+}
+
+# Create a single DB instance to back our applications.
+# NOTE: We might need multiple instances in prod, if it turns out there's too
+# much resource contention sharing a single instance.
+resource google_sql_database_instance postgres {
+  provider = google.target
+  depends_on = [module.enable_services]
+
+  name = "${var.name_prefix}-${random_id.cloudsql_random_id.hex}"
+  database_version = var.postgres_version
+
+  settings {
+    activation_policy = "ALWAYS"
+    pricing_plan = "PER_USE"
+    replication_type = "SYNCHRONOUS"
+    tier = "db-custom-${var.cpu}-${var.ram}"
+    user_labels = var.labels
+
+    backup_configuration {
+      binary_log_enabled = false
+      enabled = true
+      start_time = "06:00"
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+      require_ssl = true
+      authorized_networks {
+        name = "Broad"
+        value = "69.173.64.0/18"
+      }
+    }
+  }
+}
+
+resource random_id db-password {
+  byte_length = 16
+}
+
+resource google_sql_user db-user {
+  name = "${var.name_prefix}-manager"
+  password = random_id.db-password.hex
+  project = var.google_project
+  instance = google_sql_database_instance.postgres.name
+}
+
+resource google_sql_database db {
+  name = "${var.name_prefix}-db"
+  project = var.google_project
+  instance = google_sql_database_instance.postgres.name
+  charset = "UTF8"
+  collation = "en_US.UTF8"
+  depends_on = [google_sql_user.db-user]
+}
---
+++ templates/terraform/cloudsql/variables.tf
@@ -0,0 +1,40 @@
+variable name_prefix {
+  type: string
+  description: "Prefix to add before cloudSQL resource names."
+}
+
+variable postgres_version {
+  type: string
+  description: "The version of PostgreSQL to use."
+  default = "POSTGRES_9_6"
+}
+
+variable cpu {
+  type: number
+  description: "Number of processors allocated to the instance."
+}
+
+variable ram {
+  type: number
+  description: "Memory allocated to the instance in MiB."
+}
+
+variable labels {
+  type: map(string)
+  description = "A set of key/value user label pairs to assign to the instance."
+}
+
+variable db_names {
+  type: list(string)
+  description: "List of database names."
+}
+
+variable user_names {
+  type: list(string)
+  description = "List of user names."
+}
+
+variable vault_prefix {
+  type: string
+  description = "Path to prepend before a vault path for this module's secrets."
+}
