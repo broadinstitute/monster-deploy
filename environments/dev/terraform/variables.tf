1c2c1f8328dbceab66fd5761cb0571fabe48b7e6
Dan Moran <danmoran@broadinstitute.org>
2020-02-05 15:48:12 -0500

    DSPDC-679 Checkpoint template for processing projects.

--- environments/dev/terraform/variables.tf
+++ environments/dev/terraform/variables.tf
@@ -1,9 +1,3 @@
-variable kubeconfig_dir_path {
-  type = string
-  description = "Local path where all kubeconfigs generated in the environment should be written."
-}
-
 locals {
-  processing_kubeconfig_dir = "${var.kubeconfig_dir_path}/processing"
   vault_prefix = "secret/dsde/monster/dev"
 }
