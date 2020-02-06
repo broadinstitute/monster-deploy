9127853355785e2b6448937256f8a29a549758b8
Daniel Moran <danmoran@broadinstitute.org>
2020-02-06 11:54:02 -0500

    DSPDC-679 Consolidate Helm releases into Flux charts (#27)

--- hack/common.sh
+++ hack/common.sh
@@ -5,8 +5,10 @@
 #####
 ## Constants.
 #####
+declare -r TERRAFORM=hashicorp/terraform:0.12.20
 declare -r KUBECTL=lachlanevenson/k8s-kubectl:v1.14.10
-declare -r HELM=lachlanevenson/k8s-helm:v3.0.2
+declare -r HELM=lachlanevenson/k8s-helm:v3.0.3
+
 declare -r HELM_OPERATOR_VERSION=v1.0.0-rc8
 declare -r SECRETS_MANAGER_VERSION=release-1.0.2
 
