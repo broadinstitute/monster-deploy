3b1639bc9a846387b9a9cdc109e836a5b58e7d4c
Raaid Arshad <rarshad@broadinstitute.org>
2020-02-24 16:23:33 -0500

    add v2f service account and attach IAM for bucket

--- templates/terraform/command-center-project/service-accounts.tf
+++ templates/terraform/command-center-project/service-accounts.tf
@@ -10,3 +10,24 @@ module command_center_gke_runner_account {
   vault_path = "${local.vault_prefix}/service-accounts/gke-runner"
   roles = ["logging.logWriter", "monitoring.metricWriter", "monitoring.viewer"]
 }
+
+module v2f_writer {
+  source = "/templates/google-sa"
+  providers = {
+    google.target = google.target,
+    vault.target = vault.target
+  }
+
+  account_id = "v2f-writer"
+  display_name = " Service account to write V2F data to GCS"
+  vault_path = "${local.vault_prefix}/service-accounts/v2f-writer"
+  roles = []
+}
+
+resource google_storage_bucket_iam_member v2f_iam {
+  provider = google.target
+  bucket = "gs://variant-to-function-result-sets"
+  role = "roles/storage.objectCreator"
+  member = "serviceAccount:${module.v2f_writer.email}"
+  depends_on = [module.v2f_writer.delay]
+}
