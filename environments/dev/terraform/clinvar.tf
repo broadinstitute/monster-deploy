bfac953243f95e05ddb42c408d7d0f75b4ec1902
Raaid Arshad <rarshad@broadinstitute.org>
2020-02-26 14:33:32 -0500

    update call of processing project in clinvar

--- environments/dev/terraform/clinvar.tf
+++ environments/dev/terraform/clinvar.tf
@@ -15,10 +15,6 @@ module clinvar {
   is_production = false
   command_center_argo_account_email = module.command_center.clinvar_argo_runner_email
   region = "us-central1"
-  k8s_zone = "a"
-  k8s_scaled_cluster_max_size = 3
-  k8s_scaled_machine_type = "n1-standard-1"
-  k8s_static_cluster_size = 2
   reader_groups = ["clingendevs@broadinstitute.org"]
   jade_repo_email = "jade-k8-sa@broad-jade-dev.iam.gserviceaccount.com"
   deletion_age_days = null # FIXME: Reset back to 30 after we set up prod.
