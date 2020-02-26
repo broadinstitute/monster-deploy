d9df1a4a7727325f3fb0c92cba1f35559dc381a1
Raaid Arshad <rarshad@broadinstitute.org>
2020-02-26 13:45:33 -0500

    Revert "remove gke-runner, dataflow-launcher, artifact-uploader, and argo-runner service accounts from processing-project"
    This reverts commit 4f496902

--- templates/terraform/processing-project/service-accounts.tf
+++ templates/terraform/processing-project/service-accounts.tf
@@ -1,3 +1,16 @@
+module processing_gke_runner_account {
+  source = "/templates/google-sa"
+  providers = {
+    google.target = google.target,
+    vault.target = vault.target
+  }
+
+  account_id = "processing-gke-runner"
+  display_name = "Service account to run GKE system pods"
+  vault_path = "${var.vault_prefix}/service-accounts/gke-runner"
+  roles = ["logging.logWriter", "monitoring.metricWriter", "monitoring.viewer"]
+}
+
 module dataflow_runner_account {
   source = "/templates/google-sa"
   providers = {
@@ -9,6 +22,45 @@ module dataflow_runner_account {
   display_name = " Service account to run Dataflow jobs"
   vault_path = "${var.vault_prefix}/service-accounts/dataflow-runner"
   roles = ["dataflow.worker"]
+}
+
+module dataflow_launcher_account {
+  source = "/templates/google-sa"
+  providers = {
+    google.target = google.target,
+    vault.target = vault.target
+  }
+
+  account_id = "dataflow-launcher"
+  display_name = " Service account to launch Dataflow jobs"
+  vault_path = "${var.vault_prefix}/service-accounts/dataflow-launcher"
+  roles = ["dataflow.developer", "compute.viewer"]
+}
+
+module artifact_uploader_account {
+  source = "/templates/google-sa"
+  providers = {
+    google.target = google.target,
+    vault.target = vault.target
+  }
+
+  account_id = "artifact-uploader"
+  display_name = " Service account to upload artifacts to GCS"
+  vault_path = "${var.vault_prefix}/service-accounts/artifact-uploader"
+  roles = []
+}
+
+module argo_runner_account {
+  source = "/templates/google-sa"
+  providers = {
+    google.target = google.target,
+    vault.target = vault.target
+  }
+
+  account_id = "argo-runner"
+  display_name = "Service account to run Argo pods"
+  vault_path = "${var.vault_prefix}/service-accounts/argo-runner"
+  roles = ["container.developer"]
 }
 
 resource google_project_iam_member command_center_argo_account_iam {
