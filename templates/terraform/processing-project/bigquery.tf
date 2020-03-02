d3e8f0a595b923c0f301bf120eab0b23255aa2d7
Sarah Wessel <swessel@broadinstitute.org>
2020-03-02 10:34:13 -0500

    Update the email used by the bigquery dev dataset

--- templates/terraform/processing-project/bigquery.tf
+++ templates/terraform/processing-project/bigquery.tf
@@ -2,16 +2,16 @@ resource google_bigquery_dataset dataset {
   provider = google.target
   dataset_id = "bigquery-dataset-dev"
   friendly_name = "BigQuery Dev Dataset"
-  description = "BigQuery dataset for the Jade Repo"
+  description = "BigQuery development dataset"
   location = "US"
   default_table_expiration_ms = 604800000 # 7 days
 
   access {
     role = "roles/bigquery.dataEditor"
-    user_by_email = var.jade_repo_email
+    user_by_email = var.command_center_argo_account_email
   }
   access {
     role = "roles/bigquery.jobUser"
-    user_by_email = var.jade_repo_email
+    user_by_email = var.command_center_argo_account_email
   }
 }
