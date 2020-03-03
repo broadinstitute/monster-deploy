e7bd26b800ebecac4ee11d283750e274dcc48fe4
Daniel Moran <danmoran@broadinstitute.org>
2020-03-03 10:03:10 -0500

    Bump Argo, and remove outdated script logic. (#43)

--- hack/common.sh
+++ hack/common.sh
@@ -11,23 +11,7 @@ declare -r HELM=lachlanevenson/k8s-helm:v3.0.3
 
 declare -r HELM_OPERATOR_VERSION=v1.0.0-rc8
 declare -r SECRETS_MANAGER_VERSION=release-1.0.2
-declare -r ARGO_VERSION=v2.5.1
-
-#####
-## Get names for the processing projects registered in the environment.
-##
-## Functions like this are why the directory is named 'hack'. Assumes that
-## all processing-project clusters in the environment have information stored
-## in Vault under a common prefix, and that the names of the directories found
-## immediately under that prefix are meant to represent project names.
-#####
-function get_processing_names () {
-  local -r env=$1
-  local -r vault_prefix=secret/dsde/monster/${env}/processing-projects/
-
-  declare -ra project_names=($(vault list ${vault_prefix} | grep '/' | sed 's#/##g'))
-  echo ${project_names[@]}
-}
+declare -r ARGO_VERSION=v2.6.0
 
 #####
 ## Read the kubeconfig for a command-center GKE cluster from its expected
@@ -37,14 +21,4 @@ function get_command_center_config () {
   local -r env=$1 config_target=$2
 
   vault read -field=kubeconfig secret/dsde/monster/${env}/command-center/gke > ${config_target}
-}
-
-#####
-## Read the kubeconfig for a processing GKE cluster from its expected
-## Vault path for the target environment.
-#####
-function get_processing_config () {
-  local -r env=$1 project=$2 config_target=$3
-
-  vault read -field=kubeconfig secret/dsde/monster/${env}/processing-projects/${project}/gke > ${config_target}
 }
