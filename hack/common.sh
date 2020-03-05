19e5940fa94dc17b2144faaee41047a2c4df6c1c
Mark Sarcevicz <smark@broadinstitute.org>
2020-03-05 13:07:46 -0500

    added var

--- hack/common.sh
+++ hack/common.sh
@@ -8,7 +8,7 @@
 declare -r TERRAFORM=hashicorp/terraform:0.12.20
 declare -r KUBECTL=lachlanevenson/k8s-kubectl:v1.14.10
 declare -r HELM=lachlanevenson/k8s-helm:v3.0.3
-
+declare -ra project_names=(clinvar)
 declare -r HELM_OPERATOR_VERSION=v1.0.0-rc8
 declare -r SECRETS_MANAGER_VERSION=release-1.0.2
 declare -r ARGO_VERSION=v2.6.0
