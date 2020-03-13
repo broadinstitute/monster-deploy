1c8bb2e349f7668470104dea1cbba21cd07f782e
Raaid Arshad <rarshad@broadinstitute.org>
2020-03-13 13:44:07 -0400

    add monster_infrastructure module to environments/prod

---
+++ environments/prod/terraform/main.tf
@@ -0,0 +1,9 @@
+module monster_infrastructure {
+  source = "/templates/monster-infrastructure"
+
+  env = "prod"
+  cluster_size = 3
+  machine_type = "n1-standard-4"
+  # 4 CPU, 15 GiB of RAM.
+  db_tier = "db-custom-4-15360"
+}
