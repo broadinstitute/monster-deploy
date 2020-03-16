f98bdfea459adef3e99feb6ae03b3d367fbd0ca7
Raaid Arshad <rarshad@broadinstitute.org>
2020-03-16 14:32:37 -0400

    update jade_repo_email to switch on production, treat as a local instead

--- templates/terraform/monster-infrastructure/clinvar.tf
+++ templates/terraform/monster-infrastructure/clinvar.tf
@@ -10,7 +10,7 @@ module clinvar {
   command_center_argo_account_email = module.command_center.clinvar_argo_runner_email
   region = "us-central1"
   reader_groups = ["clingendevs@broadinstitute.org"]
-  jade_repo_email = "jade-k8-sa@broad-jade-${local.env}.iam.gserviceaccount.com"
+  jade_repo_email = local.jade_repo_email
   deletion_age_days = 14
   vault_prefix = "${local.vault_prefix}/processing-projects/clinvar"
 }
