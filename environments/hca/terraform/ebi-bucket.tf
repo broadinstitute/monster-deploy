a8839a54f4352e893d6edfb3c40a905e8fcf87ff
Dan Moran <danmoran@broadinstitute.org>
2020-06-02 10:34:21 -0400

    DSPDC-1104 Add STS permissions to EBI bucket.

--- environments/hca/terraform/ebi-bucket.tf
+++ environments/hca/terraform/ebi-bucket.tf
@@ -16,23 +16,36 @@ module ebi_writer_account {
   account_id = "ebi-staging-writer"
   display_name = "Account used by EBI to interact with their staging bucket"
   vault_path = "${local.dev_vault_prefix}/service-accounts/ebi-storage-writer"
-  roles = []
+  roles = ["storagetransfer.user", "storagetransfer.viewer"]
 }
 
-# Make sure EBI is an admin on their bucket.
+# EBI is an admin on their bucket.
 resource google_storage_bucket_iam_member ebi_writer_iam {
   provider = google-beta.target
   bucket = google_storage_bucket.ebi_staging_bucket.name
-  role = "roles/storage.admin"
+  role = "roles/storage.objectAdmin"
   member = "serviceAccount:${module.ebi_writer_account.email}"
 }
 
-# Make sure both TDRs can read from the bucket.
+# Both TDRs and our Dataflow SA can read from the bucket.
 resource google_storage_bucket_iam_member tdr_reader_iam {
   provider = google-beta.target
-  for_each = toset([local.dev_repo_email, local.prod_repo_email])
+  for_each = toset([local.dev_repo_email, local.prod_repo_email, module.hca_dataflow_account.email])
 
   bucket = google_storage_bucket.ebi_staging_bucket.name
-  role = "roles/storage.admin"
+  role = "roles/storage.objectViewer"
   member = "serviceAccount:${each.value}"
+}
+
+# Google's Storage Transfer Service can interact with the bucket.
+data google_storage_transfer_project_service_account sts_account {
+  provider = google-beta.target
+}
+
+resource google_storage_bucket_iam_member sts_iam {
+  provider = google-beta.target
+
+  bucket = google_storage_bucket.ebi_staging_bucket.name
+  role = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_storage_transfer_project_service_account.sts_account.email}"
 }
