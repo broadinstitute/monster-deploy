60d21debdd16c9ac930a5666ec2cfa386f868aeb
Raaid Arshad <rarshad@broadinstitute.org>
2020-07-29 15:21:17 -0400

    simplify hca prod and just set up buckets with permissions right now

--- environments/hca-prod/terraform/provider.tf
+++ environments/hca-prod/terraform/provider.tf
@@ -1,12 +1,11 @@
 provider google-beta {
   alias = "target"
 
-  project = local.prod_project_name
+  project = local.prod_project_id
   region = "us-central1"
 }
 provider google-beta {
   alias = "prod-core"
-
   project = "broad-dsp-monster-prod"
   region = "us-central1"
 }
--- environments/hca-prod/terraform/variables.tf
+++ environments/hca-prod/terraform/variables.tf
@@ -1,4 +1,5 @@
 locals {
+  prod_project_id = "mystical-slate-284720"
   prod_project_name = "broad-dsp-monster-hca-prod"
   prod_vault_prefix = "secret/dsde/monster/prod/ingest/hca"
   prod_repo_email = "terra-data-repository@broad-datarepo-terra-prod.iam.gserviceaccount.com"
