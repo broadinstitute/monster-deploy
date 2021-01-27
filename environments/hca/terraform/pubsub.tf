c2c271ac5181cdf04fefb0c93ef83d6844568197
Drew Herbst <aherbst@broadinstitute.org>
2021-01-27 15:22:08 -0500

    Rename topic

--- environments/hca/terraform/pubsub.tf
+++ environments/hca/terraform/pubsub.tf
@@ -2,7 +2,7 @@ module ebi_staging_notification_pubsub_topic {
   source     = "terraform-google-modules/pubsub/google"
   version    = "~>1.8"
   project_id = local.dev_project_name
-  topic      = "broad-dsp-monster-hca-dev.staging-transfer-notifications"
+  topic      = "broad-dsp-monster-hca-dev.staging-transfer-notifications.ebi"
   pull_subscriptions = [
     {
       name = "ebi"
