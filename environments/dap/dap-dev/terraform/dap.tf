908ceaf390a1a73763b3b21a42fb0e1fb967cc67
Drew Herbst <aherbst@broadinstitute.org>
2021-04-20 11:23:55 -0400

    Add terraform dirs to conform to our GH action expectations

--- environments/dap/dap-dev/terraform/dap.tf
+++ environments/dap/dap-dev/terraform/dap.tf
@@ -1,5 +1,5 @@
 module dap {
-  source       = "../base"
+  source       = "../../base"
   project_name = "broad-dsp-monster-dap-dev"
   vault_prefix = "secret/dsde/monster/dev/dog-aging/redcap-tokens/automation"
 }
