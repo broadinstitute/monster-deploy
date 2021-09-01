80201e2323e50d80b09428a56e8075ef67d27472
Drew Herbst <aherbst@broadinstitute.org>
2021-09-01 11:36:46 -0400

    Fix formatting

--- environments/dap/dap-dev/terraform/dap.tf
+++ environments/dap/dap-dev/terraform/dap.tf
@@ -1,8 +1,8 @@
 module dap {
-  source       = "../../base"
-  project_id   = "broad-dsp-monster-dap-dev"
-  project_name = "broad-dsp-monster-dap-dev"
-  vault_prefix = "secret/dsde/monster/dev/dog-aging"
+  source               = "../../base"
+  project_id           = "broad-dsp-monster-dap-dev"
+  project_name         = "broad-dsp-monster-dap-dev"
+  vault_prefix         = "secret/dsde/monster/dev/dog-aging"
   dagster_runner_email = ""
 }
 
