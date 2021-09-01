7c1d529224085e1ff9ad1690c21b7ba24572d273
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-09-01 12:05:59 -0400

    [DSPDC-1891] Add K8S monster dagster role bindings (#160)
    Why
    
    We need IAM users for the monster "command center" dagster K8S service accounts so we can grant them the appropriate
    permissions in the DAP GCP project.
    
    This PR
    
    Adds the role binding terraform

---
+++ environments/prod/terraform/dagster.tf
@@ -0,0 +1,28 @@
+module dagster_runner_service_account {
+  source = "../../../templates/terraform/google-sa"
+  providers = {
+    google.target = google.command-center,
+    vault.target  = vault.command-center
+  }
+
+  account_id   = "monster-dagster-runner"
+  display_name = "Service account to run Dagster pipelines."
+  vault_path   = "${local.vault_prefix}/service-accounts/dagster-runner"
+  roles = [
+    "dataflow.developer",
+    "compute.viewer",
+    "bigquery.jobUser",
+    "bigquery.dataOwner",
+  ]
+}
+
+resource google_service_account_iam_binding kubernetes_role_binding {
+  provider = google.command-center
+
+  service_account_id = module.dagster_runner_service_account.id
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:${local.prod_project_name}.svc.id.goog[dagster/monster-dagster]"
+  ]
+}
