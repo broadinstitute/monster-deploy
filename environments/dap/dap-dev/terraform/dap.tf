bad94b99d2386b532576aa79463eb4e9e628a043
Drew Herbst <aherbst@broadinstitute.org>
2021-09-01 12:28:32 -0400

    Add dagster runner sa emails

--- environments/dap/dap-dev/terraform/dap.tf
+++ environments/dap/dap-dev/terraform/dap.tf
@@ -3,7 +3,7 @@ module dap {
   project_id           = "broad-dsp-monster-dap-dev"
   project_name         = "broad-dsp-monster-dap-dev"
   vault_prefix         = "secret/dsde/monster/dev/dog-aging"
-  dagster_runner_email = ""
+  dagster_runner_email = "monster-dagster-runner@broad-dsp-monster-dev.iam.gserviceaccount.com"
 }
 
 terraform {
