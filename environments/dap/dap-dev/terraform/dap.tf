7d3ce325b774121c80eae401b2eb397335b4525f
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-09-02 13:41:26 -0400

    [DSPDC-1891] Add dagster helmfile (#159)
    Why
    
    
    We've been using a helmfile template to instantiate and update dagster clusters in HCA dev + prod as
    well as monster dev + prod. We should capture that in source control with instructions for use.
    
    This PR
    
    Adds the helmfile and instructions for use

--- environments/dap/dap-dev/terraform/dap.tf
+++ environments/dap/dap-dev/terraform/dap.tf
@@ -1,7 +1,9 @@
 module dap {
-  source       = "../../base"
-  project_id   = "broad-dsp-monster-dap-dev"
-  project_name = "broad-dsp-monster-dap-dev"
+  source               = "../../base"
+  project_id           = "broad-dsp-monster-dap-dev"
+  project_name         = "broad-dsp-monster-dap-dev"
+  vault_prefix         = "secret/dsde/monster/dev/dog-aging"
+  dagster_runner_email = "monster-dagster-runner@broad-dsp-monster-dev.iam.gserviceaccount.com"
 }
 
 terraform {
