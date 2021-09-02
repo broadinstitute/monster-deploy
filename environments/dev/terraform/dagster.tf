85c660e552e87ae36a98531a8cb6e19a0dfde8b2
Drew Herbst <aherbst@broadinstitute.org>
2021-09-02 13:29:08 -0400

    Add dagster temp storage buckets

--- environments/dev/terraform/dagster.tf
+++ environments/dev/terraform/dagster.tf
@@ -26,3 +26,31 @@ resource google_service_account_iam_binding kubernetes_role_binding {
     "serviceAccount:${local.dev_project_name}.svc.id.goog[dagster/monster-dagster]"
   ]
 }
+
+# Bucket for storing intermediate results from Dagster pipeline runs.
+resource google_storage_bucket dev_dagster_storage {
+  provider = google.command-center
+  name     = "${local.dev_project_name}-dagster-storage"
+  location = "US"
+
+  # delete intermediate results after four weeks
+  lifecycle_rule {
+    condition {
+      age = 28
+    }
+
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource google_storage_bucket_iam_member dev_dagster_run_bucket_iam {
+  provider = google.command-center
+  bucket   = google_storage_bucket.dev_dagster_storage.name
+  # When the storage.admin role is applied to an individual bucket,
+  # the control applies only to the specified bucket and objects within
+  # the bucket: https://cloud.google.com/storage/docs/access-control/iam-roles
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${module.dagster_runner_service_account.email}"
+}
