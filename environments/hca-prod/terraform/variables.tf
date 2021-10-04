4d204cec8b4e8b9ec80270de6351a9dc8dd76811
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-10-04 12:47:03 -0400

    Housekeeping (#170)
    * Add perms for the dagster runner against the ebi dev staging bucket
    
    * Update etl partitioning bucket name, add dagster runner perms to ebi staging bucket
    
    * Updates
    
    * Create partitions bucket in prod
    
    * Give the jade real prod SA access to the prod temp bucket
    
    * Remove more grafana references

--- environments/hca-prod/terraform/variables.tf
+++ environments/hca-prod/terraform/variables.tf
@@ -1,7 +1,8 @@
 locals {
-  prod_project_id   = "mystical-slate-284720"
-  prod_project_name = "broad-dsp-monster-hca-prod"
-  prod_vault_prefix = "secret/dsde/monster/prod/ingest/hca"
-  dev_repo_email    = "jade-k8-sa@broad-jade-dev.iam.gserviceaccount.com"
-  prod_repo_email   = "terra-data-repository@broad-datarepo-terra-prod.iam.gserviceaccount.com"
+  prod_project_id      = "mystical-slate-284720"
+  prod_project_name    = "broad-dsp-monster-hca-prod"
+  prod_vault_prefix    = "secret/dsde/monster/prod/ingest/hca"
+  dev_repo_email       = "jade-k8-sa@broad-jade-dev.iam.gserviceaccount.com"
+  prod_repo_email      = "terra-data-repository@broad-datarepo-terra-prod.iam.gserviceaccount.com"
+  real_prod_repo_email = "datarepo-jade-api@terra-datarepo-production.iam.gserviceaccount.com"
 }
